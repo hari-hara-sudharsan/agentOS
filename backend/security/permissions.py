9ea0b3d020d898300e88ba93bd236d85d41a3f82
hari-hara-sudharsan <sudharsanj008gmail.com>
2026-03-08 15:05:38 +0530

    Updated permissions python file to the security folder

---
+++ backend/security/permissions.py
@@ -0,0 +1,14 @@
+ALLOWED_ACTIONS = [
+    "read_gmail",
+    "send_slack_message",
+    "create_calendar_event",
+    "upload_to_drive"
+]
+
+
+def check_permission(action):
+
+    if action not in ALLOWED_ACTIONS:
+        raise Exception("Action not allowed")
+
+    return True
