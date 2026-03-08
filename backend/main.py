563366f575353dd059e96a53574f099fb9b98175
hari-hara-sudharsan <sudharsanj008gmail.com>
2026-03-08 15:51:47 +0530

    Modified the main file

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from api.routes import router as api_router
 
+from database.db import engine
+from database.models import Base
+
 app = FastAPI(title="AgentOS Backend")
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(api_router, prefix="/api")
 
