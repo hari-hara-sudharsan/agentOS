a686487bbb2b654af1256880f2abd5c0e6ca1ff4
hari-hara-sudharsan <sudharsanj008gmail.com>
2026-03-08 18:26:47 +0530

    Added AuthProvider file inside components folder

---
+++ frontend/src/components/AuthProvider.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { Auth0Provider } from "@auth0/auth0-react"
+
+export default function AuthProvider({ children }: any) {
+
+  return (
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN!}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!}
+      authorizationParams={{
+        audience: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE,
+        redirect_uri: typeof window !== "undefined" ? window.location.origin : undefined
+      }}
+    >
+      {children}
+    </Auth0Provider>
+  )
+}
