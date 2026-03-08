b0d4305394b2f0cf0ef3ef04b53cda9b8d17766b
hari-hara-sudharsan <sudharsanj008gmail.com>
2026-03-08 16:10:40 +0530

    Modified router files

--- backend/router/task_router.py
+++ backend/router/task_router.py
@@ -1,8 +1,5 @@
 from agents.api_agent import APIAgent
 from agents.browser_agent import BrowserAgent
-from agents.planner_agent import create_plan
-from database.activity_logger import log_activity
-from memory.execution_memory import ExecutionMemory
 
 
 class TaskRouter:
@@ -11,61 +8,15 @@ class TaskRouter:
 
         self.api_agent = APIAgent()
         self.browser_agent = BrowserAgent()
-        self.memory = ExecutionMemory()
 
-    def execute(self, user_message, user_context):
+    def execute_tool(self, task, user_context, memory):
 
-        # Step 1: create plan
-        plan = create_plan(user_message)
+        tool = task["tool"]
 
-        tasks = plan.get("tasks", [])
+        if tool.startswith("browser_"):
 
-        results = []
+            return self.browser_agent.execute(task, memory)
 
-        # Step 2: execute tasks
-        for task in tasks:
+        else:
 
-            tool = task["tool"]
-
-            try:
-
-                if tool.startswith("browser_"):
-
-                    result = self.browser_agent.execute(task, self.memory)
-
-                else:
-
-                    result = self.api_agent.execute(task, user_context, self.memory)
-
-                self.memory.store(tool, result)
-
-                log_activity(
-                    user_context["sub"],
-                    tool,
-                    "success"
-                )
-
-                results.append({
-                    "tool": tool,
-                    "status": "success",
-                    "result": result
-                })
-
-            except Exception as e:
-
-                log_activity(
-                    user_context["sub"],
-                    tool,
-                    "failed"
-                )
-
-                results.append({
-                    "tool": tool,
-                    "status": "failed",
-                    "error": str(e)
-                })
-
-        return {
-            "goal": plan.get("goal"),
-            "steps_executed": results
-        }
+            return self.api_agent.execute(task, user_context, memory)
