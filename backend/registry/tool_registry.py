d384b49c1063e11561180252a789591a229112ee
hari-hara-sudharsan <sudharsanj008gmail.com>
2026-03-08 16:27:12 +0530

    added tool registry file to the registry folder

---
+++ backend/registry/tool_registry.py
@@ -0,0 +1,20 @@
+class ToolRegistry:
+
+    def __init__(self):
+
+        self.tools = {}
+
+    def register(self, name, func):
+
+        self.tools[name] = func
+
+    def get(self, name):
+
+        return self.tools.get(name)
+
+    def list_tools(self):
+
+        return list(self.tools.keys())
+
+
+tool_registry = ToolRegistry()
