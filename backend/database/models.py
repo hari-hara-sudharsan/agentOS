558ee8d2dbd22711b1fcb6f8d3feade0dd8e5ca3
hari-hara-sudharsan <sudharsanj008gmail.com>
2026-03-10 19:29:16 +0530

    Updated model file to store service token per user

--- backend/database/models.py
+++ backend/database/models.py
@@ -17,3 +17,15 @@ class ActivityLog(Base):
     status = Column(String)
 
     timestamp = Column(DateTime, default=datetime.utcnow)
+
+class Integration(Base):
+
+    __tablename__ = "integrations"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    user_id = Column(String)
+
+    service = Column(String)
+
+    access_token = Column(String)
