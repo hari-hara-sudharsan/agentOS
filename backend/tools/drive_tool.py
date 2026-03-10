dff1687ec4c023859ada219cedf79035e2b60b50
hari-hara-sudharsan <sudharsanj008gmail.com>
2026-03-10 19:22:31 +0530

    Updated drive tools file for token vault

--- backend/tools/drive_tool.py
+++ backend/tools/drive_tool.py
@@ -1,11 +1,17 @@
 from registry.tool_registry import tool_registry
+from integrations.token_vault import get_service_token
 
 import requests
 
 
 def upload_to_drive(user_context, params):
 
-    token = user_context.get("drive_token")
+    user_id = user_context["sub"]
+
+    token = get_service_token(
+        "https://www.googleapis.com",
+        user_id
+    )
 
     file_path = params.get("file_path")
 
