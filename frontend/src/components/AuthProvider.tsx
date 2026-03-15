9949f5f3f0a29e4c5e296aad50d65ead32392c67
hari-hara-sudharsan <sudharsanj008gmail.com>
2026-03-15 12:38:17 +0530

    Configure Auth0 token refresh

--- frontend/src/components/AuthProvider.tsx
+++ frontend/src/components/AuthProvider.tsx
@@ -8,6 +8,8 @@ export default function AuthProvider({ children }: any) {
     <Auth0Provider
       domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN!}
       clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!}
+      cacheLocation="localstorage"
+      useRefreshTokens={true}
       authorizationParams={{
         audience: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE,
         redirect_uri: typeof window !== "undefined" ? window.location.origin : undefined
