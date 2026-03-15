651a75eff1bda29641b10b728903767323736781
hari-hara-sudharsan <sudharsanj008gmail.com>
2026-03-15 11:38:33 +0530

    Updated Security folder by adding roles python file

---
+++ backend/security/roles.py
@@ -0,0 +1,17 @@
+ROLES = {
+    "admin": [
+        "read_gmail",
+        "send_slack_message",
+        "upload_to_drive",
+        "create_calendar_event",
+        "browser_automation"
+    ],
+    "power_user": [
+        "read_gmail",
+        "send_slack_message",
+        "create_calendar_event"
+    ],
+    "basic_user": [
+        "read_gmail"
+    ]
+}
